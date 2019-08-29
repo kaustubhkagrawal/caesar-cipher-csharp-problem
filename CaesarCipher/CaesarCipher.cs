844e7bb8311a0258880467c318bfd036ff347eed
kaustubh.agrawal <kaustubh.agrawal@cgi.com>
2019-08-29 16:03:35 +0530

    Problem solved without use of any collection and LINQ

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
+
 
 namespace CaesarCipher
 {
     public static class RotationalCipher
     {
+        private static char SkipChar(char c, int jump)
+        {
+
+            if(!char.IsLetter(c))
+            {
+                return c;
+            }
+            char a = char.IsUpper(c) ? 'A' : 'a';
+            int startpoint = (int)a;
+            int pos = (int)c;
+
+            return (char)(startpoint + (pos - startpoint + jump) % 26 );
+        }
+
+        /*private char */
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            /*throw new NotImplementedException("You need to implement this function.");*/
+
+
+            string sb = "";
+
+            foreach(char c in text)
+            {
+                sb += SkipChar(c, shiftKey);
+            }
+
+            return sb;
         }
+
+
         public static void Main(string[] args){
             
         }
